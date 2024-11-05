5c6b5572a349327fd816b35116377d56038bd481
Nicolas Mango <nicolas_mango@hotmail.com>
2024-11-04 23:31:51 -0300

    nueva version del modelo y modificaciones en front

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ label_encoder = joblib.load('label_encoder.pkl')
 
 @app.route('/',methods=['GET'])
 def form():
-    return render_template('form.html')
+    return render_template('formv3.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -17,13 +17,19 @@ def predict():
     print(data)
     genre = data['genre_encoded']
     hours = data['Single-Player_Main Story_Average']
+    review = 100
     print(genre)
     new_genre_encoded = label_encoder.transform([genre])[0]
     print(new_genre_encoded)
-    features = np.array([[new_genre_encoded, hours]])
+    features = np.array([[new_genre_encoded, hours,review]])
     print(features)
-    prediction = model.predict(pd.DataFrame(features))
-    return render_template("form.html" , prediction=prediction)
+    #prediction = model.predict(pd.DataFrame(features))
+    #prediction = model.kneighbors(pd.DataFrame(features), n_neighbors=5)
+    # Obtener las 5 recomendaciones más cercanas
+    distances, indices = model.kneighbors(pd.DataFrame(features), n_neighbors=5)
+# Obtener los nombres de los juegos recomendados como un array
+    prediction = [model.game_names[i] for i in indices[0]]  # y_train contiene los nombres de los juegos
+    return render_template("formv3.html" , prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
